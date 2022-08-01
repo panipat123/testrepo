01a7d764dc9ed8d205108156f43d98ad917237a5
panipat123 <102275798+panipat123@users.noreply.github.com>
2022-08-01 19:34:58 +0530

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -1,3 +1,4 @@
 from selenium import  webdriver
 driver = webdriver.Chrome(executable_path="C:\\Users\\palso\\Downloads\\chromedriver_win32 (3)\\chromedriver.exe")
 driver.get("https://www.naukri.com/")
+driver.maximize()
